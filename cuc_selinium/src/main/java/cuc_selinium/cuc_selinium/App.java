009501b14dcaa8a05c413f6544c6d77954381e74
sriravali <ravali9sri@gmail.com>
2019-04-12 21:19:33 +0530

    committing all

---
+++ cuc_selinium/src/main/java/cuc_selinium/cuc_selinium/App.java
@@ -0,0 +1,13 @@
+package cuc_selinium.cuc_selinium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cuc_selinium/src/main/java/runner/testrunner.java
@@ -0,0 +1,26 @@
+package runner;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import com.cucumber.listener.ExtentCucumberFormatter;
+import com.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/java/Features",plugin = { "pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},monochrome = true,glue= {"seleniumgluecode"}
+)
+
+public class testrunner {
+	
+	@AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("src/main/java/config/configur.xml"));
+    
+    }
+	
+}
