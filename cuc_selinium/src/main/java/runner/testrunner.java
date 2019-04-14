841dc6f8194e7e0ca5d49df63cd7349a79f2b532
sriravali <ravali9sri@gmail.com>
2019-04-14 23:20:52 +0530

    modified changes

--- cuc_selinium/src/main/java/runner/testrunner.java
+++ cuc_selinium/src/main/java/runner/testrunner.java
@@ -1,25 +1,25 @@
 package runner;
 
 import java.io.File;
-
+import org.junit.*;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
-import com.cucumber.listener.ExtentCucumberFormatter;
-import com.cucumber.listener.Reporter;
+//import com.cucumber.listener.ExtentCucumberFormatter;
+//import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue= {"seleniumgluecode"},features = "src/main/java/Features",monochrome = true,plugin = { "pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"})
+@CucumberOptions(glue= {"seleniumgluecode"},features = "src/main/java/Features")
 
 public class testrunner {
 	
-	@AfterClass
-    public static void writeExtentReport() {
-        Reporter.loadXMLConfig(new File("src/main/java/config/configur.xml"));
-    
-    }
+//	@AfterClass
+//    public static void writeExtentReport() {
+//        Reporter.loadXMLConfig(new File("src/main/java/config/configur.xml"));
+//    
+//    }
 	
 }
