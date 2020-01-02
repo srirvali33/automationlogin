70117ea6492e1cecc7f1e490c99ed4920a786497
srirvali33 <ravali9sri@gmail.com>
2020-01-02 10:25:18 +0530

    commiting changed code

--- cuc_selinium/src/main/java/seleniumgluecode/test.java
+++ cuc_selinium/src/main/java/seleniumgluecode/test.java
@@ -2,7 +2,11 @@ package seleniumgluecode;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -18,13 +22,14 @@ public class test {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver","sourcefiles/chromedriver.exe");
 		driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://automationpractice.com/index.php");
     }
 	
     
 	@When("^user navigates to Login Page$")
     public void user_navigates_to_Login_Page() throws Throwable {
+		
         driver.findElement(By.linkText("Sign in")).click();
     }
     
@@ -41,5 +46,45 @@ public class test {
     	String actual = driver.findElement(By.cssSelector(".info-account")).getText();
         Assert.assertEquals(exp_message, actual);
         driver.quit();  
-    }      
+    }
+    
+    @Given("^User navigates to stackoverflow website$")
+	public void user_navigates_to_stackoverflow_website() throws Throwable {
+    	System.setProperty("webdriver.chrome.driver","sourcefiles/chromedriver.exe");
+		driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://stackoverflow.com/");
+	}
+
+	@And("^User clicks on the login button on homepage$")
+	public void user_clicks_on_the_login_button_on_homepage() throws Throwable {
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
+	}
+
+	@And("^User enters a valid username$")
+	public void user_enters_a_valid_username() throws Throwable {
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("autotestudemy@mail.com");
+	}
+
+	@And("^User enters a valid password$")
+	public void user_enters_a_valid_password() throws Throwable {
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Password321!");
+	}
+
+	@When("^User clicks on the login button$")
+	public void user_clicks_on_the_login_button() throws Throwable {
+		driver.findElement(By.xpath(".//*[@id='submit-button']")).click();
+	}
+
+	@Then("^User should be taken to the successful login page$")
+	public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
+		Thread.sleep(3000);
+		WebElement askQuestionButton = driver.findElement(By.xpath("//a[contains(text(), 'Ask Question')]"));
+		Assert.assertEquals(true, askQuestionButton.isDisplayed());
+		driver.quit();
+	}
+	
+
 }
